ac1d7162a148e0a13b37812ceac8ce6704a59d4c
lazar <lazav94@gmail.com>
2019-04-22 11:13:34 +0200

    eslint update

---
+++ server/server.js
@@ -0,0 +1,26 @@
+// Configuration of express app
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+require("dotenv").config(); // This will put .env to process variables
+
+const app = express();
+
+// Setting up the application port
+app.set("port", process.env.PORT || 8081);
+
+app.use(express.static(path.join(__dirname, "public")));
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+
+// Database configuration
+require("./services/dbconfig");
+
+// Routes configuration
+// require('./routes')(app);
+
+app.listen(app.get("port"), () => {
+  console.log("🖥  Server is 🏃  on port:", app.get("port"));
+});
+
+module.exports = app;
---
+++ server/services/dbconfig.js
@@ -0,0 +1,1 @@
+// Mongo/Dynamo/MySql config
